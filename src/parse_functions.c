1cc631535325630e220a2e0524e71441b9c109ef
Brendan Prado <bprado@f1r4s3.codam.nl>
2019-11-22 18:17:14 +0100

    have realized that va_arg must pass its output in the parse function, as I cannot upcast as I intended in parse_length

--- src/parse_functions.c
+++ src/parse_functions.c
@@ -6,63 +6,45 @@
 /*   By: bprado <bprado@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/03 19:16:18 by bprado         #+#    #+#                */
-/*   Updated: 2019/11/21 20:53:55 by bprado        ########   odam.nl         */
+/*   Updated: 2019/11/22 18:05:12 by bprado        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	parse_flags(t_pf_object *obj)
-{
-	while (ft_strchr_int("#0- +", obj->str[obj->i_str]) != -1)
-		obj->flags |= 1 << ft_strchr_int("#0- +", obj->str[obj->i_str++]);
-	if (obj->flags & ZERO_F && obj->flags & MINUS_F)
-		obj->flags ^= ZERO_F;
-	if (obj->flags & SPACE_F && obj->flags & PLUS_F)
-		obj->flags ^= SPACE_F;
-}
 
-void	parse_width_precision(t_pf_object *obj)
-{
-	obj->width = ft_atoi(&(obj->str[obj->i_str]));
-	while (ft_isdigit(obj->str[obj->i_str]))
-		++obj->i_str;
-	if (obj->str[obj->i_str] == '.')
-	{
-		obj->i_str++;
-		obj->flags |= PRECISN;
-		obj->prcs = ft_atoi(&(obj->str[obj->i_str]));
-		while (ft_isdigit(obj->str[obj->i_str]))
-			++obj->i_str;
-	}
-}
-
+/*
 void	parse_length(t_pf_object *obj, char flip)
 {
-	if (flip)
+	// if (flip)
+	// {
+	// 	if (obj->str[obj->i_str] == 'l')
+	// 		obj->flags |= (obj->str[obj->i_str + 1] == 'l') ? LL_F : L_F;
+	// 	if (obj->str[obj->i_str] == 'l')
+	// 		obj->i_str += (obj->str[obj->i_str + 1] == 'l') ? 2 : 1;
+	// 	if (obj->str[obj->i_str] == 'h')
+	// 		obj->flags |= (obj->str[obj->i_str + 1] == 'h') ? HH_F : H_F;
+	// 	if (obj->str[obj->i_str] == 'h')
+	// 		obj->i_str += (obj->str[obj->i_str + 1] == 'h') ? 2 : 1;
+	// 	if (obj->str[obj->i_str] && obj->str[obj->i_str] == 'L')
+	// 		obj->flags |= CAP_L_F;
+	// }
+	else if (obj->flags & 0x1E0)
 	{
-		if (obj->str[obj->i_str] == 'l')
-			obj->flags |= (obj->str[obj->i_str + 1] == 'l') ? LL_F : L_F;
-		if (obj->str[obj->i_str] == 'l')
-			obj->i_str += (obj->str[obj->i_str + 1] == 'l') ? 2 : 1;
-		if (obj->str[obj->i_str] == 'h')
-			obj->flags |= (obj->str[obj->i_str + 1] == 'h') ? HH_F : H_F;
-		if (obj->str[obj->i_str] == 'h')
-			obj->i_str += (obj->str[obj->i_str + 1] == 'h') ? 2 : 1;
-		if (obj->str[obj->i_str] && obj->str[obj->i_str] == 'L')
-			obj->flags |= CAP_L_F;
-	}
-	else
-	{
-		obj->val.ll = obj->flags & H_F ? (short)obj->val.ll : obj->val.ll;
-		obj->val.ll = obj->flags & HH_F ?
-									(unsigned char)obj->val.ll : obj->val.ll;
-		obj->val.ll = obj->flags & L_F ? (long)obj->val.ll : obj->val.ll;
-		obj->val.ll = obj->flags & LL_F ? (long long)obj->val.ll : obj->val.ll;
+		obj->val.ll = obj->flags & H_F ? va_arg(obj->ap, short) : 0;
+		obj->val.ll = obj->flags & HH_F ? va_arg(obj->ap, char) : 0;
+		obj->val.ll = obj->flags & L_F ? (long)obj->val.ll : 0;
+		obj->val.ll = obj->flags & LL_F ? (long long)obj->val.ll : 0;
 		if (obj->flags & CAP_L_F)
 			obj->val.lngdbl = (long double)obj->val.ll;
 	}
 }
+		d, i			o, u, x, X
+	hh	signed char		unsigned char
+	h	short int		unsigned short int
+	l	long int		unsigned long int
+	ll	long long int	unsigned long long int
+ */
 
 void	parse_specifier(t_pf_object *obj)
 {
@@ -91,8 +73,30 @@ void	parse_specifier(t_pf_object *obj)
 
 void	parse_general(t_pf_object *obj)
 {
-	parse_flags(obj);
-	parse_width_precision(obj);
-	parse_length(obj, 1);
+	while (ft_strchr_int("#0- +", obj->str[obj->i_str]) != -1)
+		obj->flags |= 1 << ft_strchr_int("#0- +", obj->str[obj->i_str++]);
+	if (obj->flags & ZERO_F && obj->flags & MINUS_F)
+		obj->flags ^= ZERO_F;
+	if (obj->flags & SPACE_F && obj->flags & PLUS_F)
+		obj->flags ^= SPACE_F;
+	obj->width = ft_atoi(&(obj->str[obj->i_str]));
+	while (ft_isdigit(obj->str[obj->i_str]))
+		++obj->i_str;
+	if (obj->str[obj->i_str] == '.')
+	{
+		obj->i_str++;
+		obj->flags |= PRECISN;
+		obj->prcs = ft_atoi(&(obj->str[obj->i_str]));
+		while (ft_isdigit(obj->str[obj->i_str]))
+			++obj->i_str;
+	}
+	if (obj->str[obj->i_str] == 'l')
+		obj->flags |= (obj->str[obj->i_str + 1] == 'l') ? LL_F : L_F;
+	if (obj->str[obj->i_str] == 'h')
+		obj->flags |= (obj->str[obj->i_str + 1] == 'h') ? HH_F : H_F;
+	if (obj->str[obj->i_str] && obj->str[obj->i_str] == 'L')
+		obj->flags |= CAP_L_F;
+	obj->i_str += obj->flags & 0x180 ? 2 : 0;
+	obj->i_str += obj->flags & 0x460 ? 1 : 0;
 	parse_specifier(obj);
 }
